0636e34fece9f1c0dea6220f0cf88e0682450623
Kévin THIERRY <kevin.thierry@open.eurogiciel.org>
2014-03-21 15:32:52 +0100

    Scripts to report the status of opened reviews
    Also add a list of all available statuses and update the install
    script.
    
    Note that the manifest file needs to be updated to document those new
    scripts.
    
    There is a bug in the script get-gerrit-status-open which causes
    https://review.tizen.org/gerrit/#/c/16751/ not to be processed. This is
    probably due to an unescaped character.
    
    Signed-off-by: Kévin THIERRY <kevin.thierry@open.eurogiciel.org>

--- install.sh
+++ install.sh
@@ -2,21 +2,23 @@
 
 if [ "$#" -ne 1 ]
 then
-    echo ""
+    echo
     echo "Copy the scripts in the given directory"
-    echo ""
+    echo
     echo "Usage: \"./install.sh <destination>\""
-    echo ""
+    echo
     exit -1
 fi
 
 BINDIR=$1
 
-install scripts/gerrit/get-gerrit-info $BINDIR
-install scripts/global/get-status $BINDIR
-install scripts/global/get-status-loop $BINDIR
-install scripts/global/get-status-stats $BINDIR
-install scripts/repo/get-commit $BINDIR
-install scripts/repo/get-repo-status $BINDIR
+# Gerrit elated scripts (Gerrit querries)
+install scripts/gerrit/* $BINDIR
+
+# Git repo related scripts
+install scripts/repo/* $BINDIR
+
+# Global scripts
+install scripts/global/* $BINDIR
 
 exit 0
